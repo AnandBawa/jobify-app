2e3fc4a9131c96a95cec7fb3e368d5bef4700aa2
Anand Bawa <anand.bawa@yahoo.com>
2023-09-24 04:35:29 +0530

    commits

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -8,14 +8,14 @@ import {
 
 const router = Router();
 
-const apiLimier = rateLimiter({
+const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000,
   max: 15,
   message: { msg: "IP rate limit exceeded. Retry in 15 minutes" },
 });
 
-router.post("/login", apiLimier, validateLoginInput, login);
-router.post("/register", apiLimier, validateRegisterInput, register);
+router.post("/login", apiLimiter, validateLoginInput, login);
+router.post("/register", apiLimiter, validateRegisterInput, register);
 router.get("/logout", logout);
 
 export default router;
